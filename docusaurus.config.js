43131d49bdc6dbd697d1b33b5d76f9ea1a2370a1
Thijs Van der Schaeghe <thijs@catlab.be>
2025-06-16 23:37:44 +0200

    Disable other languages for now.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
 
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'nl', 'fr', 'de', 'es' ],
+    locales: ['en', 'nl' /*, 'fr', 'de', 'es' */ ],
     path: 'i18n',
   },
 
